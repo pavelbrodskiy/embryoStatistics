000c4dfda8a125942868a3b7c0521d3c572a35c2
Sean Keenan <skeenan@nd.edu>
2015-11-22 21:15:56 -0500

    updated findNeighbors.m name (findNeigbors.m before) added ginputc.m to fluorStatAnalyzer.m to replace ginput.m for GUI point selection Changed ginputc.m default colors to make them higher contrast with embryo images Not finished working with the lines from ginputc but a lot of progress made

--- fluorStatAnalyzer.m
+++ fluorStatAnalyzer.m
@@ -1,45 +1,52 @@
-%Color Map Analysis
-%This script converts seedwater color map output into data
+%% fluorStatAnalyzer.m
+% Color Map Analysis
+% Updated 9/16/15
+%
+% This script converts seedwater color map output into data
+% open fluorescent data which has already been stitched, so we have our
+% images and stains, we have cell boundaries and things are already
+% segmented so we have label matrix that says where cells are
+% Now we do analysis - find which cells are adjacent to which cells,
+% expression levels of proteins, shapes/sizes of cells
 
-clear cells
+% want to ask the question: over the course of a compartment, does the
+% front have bigger/same/smaller cell areas than back?
 
-%% Inputs
-inputfolder = ['Data' filesep];
-%pixelRatio = 2.919708029; % 2.919708029 => 40x
-pixelRatio = 4.280821918; % 4.280821918 => 60x
-%mapName = 'Embryo 1 cycE PH3 Stage 13 7Nov14 modified.png';
-%fluorName1 = 'Embryo 1 PH3 en cycE.tif';
-%fluorName2 = 'Embryo 1 GFP en cycE.tif';
-%compositeName = 'Composite (RGB).tif';
-%segmentationName = 'b.tiff';
-
-mapName = 'Embryo 1 Colormap.tif';
-% fluorName1 = 'dpERK.tif';
-% fluorName2 = 'engrailed.tif';
-
-
-
-compositeName = 'Embryo 1.bmp';
-% segmentationName = 'cell boundaries.tif';
-
-% segmentationFluor = imresize(imread([inputfolder segmentationName]),size(label));
+%% Inputs - open files
+inputfolder = '~/Desktop/Zartman Research/Junior/Fluorescence Analysis/09.1.15 analysis code';
+pixelRatio = 2.919708029; % 2.919708029 => 40x
+mapName = 'Segmentation Map Modified.png';
+fluorName1 = 'dpERK.tif';
+fluorName2 = 'engrailed.tif';
+compositeName = 'composite.tif';
+segmentationName = 'cell boundaries.tif';
+segmentationFluor = imread([inputfolder, '/', segmentationName]);
 dateString = datestr(now);
 dateString (dateString == ':') = '.';
 mapScale = 3;
 
 % segmentListMaster = {'Head';'Amnioserosa';'Ventral Segments T1 through A1'; ...
-%     'Ventral Segments A2 through a5';'Ventral Segments A6 through A9';'C5'; ...
-%     'dorsal T1';'dorsal T2';'dorsal T3';'dorsal A1';'dorsal A2';'dorsal A3'; ...
-%     'dorsal A4';'dorsal A5'};
-% 
-% %fbMAT = {''};
-% fbMAT = {' front'; ' back'};
-% 
-% APMAT = {'Anterior'; 'Posterior'};
-% segmentListMaster = {'Head', 'Amnioserosa'};
+%      'Ventral Segments A2 through a5';'Ventral Segments A6 through A9';'C5'; ...
+%      'dorsal T1';'dorsal T2';'dorsal T3';'dorsal A1';'dorsal A2';'dorsal A3'; ...
+%      'dorsal A4';'dorsal A5'};
+
+%fbMAT = {''};
+%fbMAT = {' front'; ' back'};
+
+%APMAT = {'Anterior'; 'Posterior'};
+
+% for segA = 1:10
+%     for AP = 1:2
+%         %for fb = 1:2
+%             %tempSTR = ['A' int2str(segA) ' - ' APMAT{AP} fbMAT{fb}]
+%             tempSTR = ['Seg' int2str(segA) ' - ' APMAT{AP}];
+%             segmentListMaster = [segmentListMaster {tempSTR}];
+%         %end
+%     end
+% end
 
 APMAT = {'Anterior'; 'Posterior'};
-segmentListMaster = {};
+segmentListMaster = {'Head', 'Amnioserosa'};
 for segA = 1:12
     for AP = 1:2
         %for fb = 1:2
@@ -49,60 +56,36 @@ for segA = 1:12
         %end
     end
 end
-
-% for segA = 1:10
-%     for AP = 1:2
-%         for fb = 1:2
-%         %tempSTR = ['A' int2str(segA) ' - ' APMAT{AP} fbMAT{fb}]
-%         tempSTR = ['Seg' int2str(segA) ' - ' APMAT{AP} fbMAT{fb}];
-%         segmentListMaster = [segmentListMaster {tempSTR}];
-%         end
-%     end
-% end
-
-
-%segmentListMaster = {'Head';'Body'};
+            
+% segmentListMaster = {'Head';'Body'};
 
 % Initialize output folder
-outputfolder = [inputfolder 'Segmentation Analysis Output ' dateString '\'];
+outputfolder = [inputfolder, '/' 'Segmentation Analysis Output ' dateString '\'];
 mkdir(outputfolder);
 
-% Import matricies
-Map = imread([inputfolder mapName]);
-%Map = label;
+%% Import matricies (read in images)
+Map = imread([inputfolder, '/', mapName]); % segmentation map
+fluorImage1 = imread([inputfolder, '/', fluorName1]);
+fluorImage2 = imread([inputfolder, '/', fluorName2]);
 colorByNumbers = labelMaker(Map);
-colorByNumbers(colorByNumbers == 0) = max(colorByNumbers(:)) + 1;
 cellsNumber = max(colorByNumbers(:)) - 1;
-% compositeMap(:,:,1) = fluorImage1;
-% compositeMap(:,:,2) = fluorImage2;
-% compositeMap(:,:,3) = segmentationFluor;
-compositeMap = imread([inputfolder compositeName]);
-fluorImage1 = squeeze(compositeMap(:,:,1));
-fluorImage2 = squeeze(compositeMap(:,:,2));
+compositeMap = imread([inputfolder, '/', compositeName]);
 
-compositeMap = uint8(compositeMap);
-fluorImage1 = imresize(fluorImage1, size(colorByNumbers));
-fluorImage2 = imresize(fluorImage2, size(colorByNumbers));
-
-%% Obtain Region Properties
-
+%% Obtain Region Properties (morphometry, shapes/sizes, centroid)
 STATS = regionprops(colorByNumbers, fluorImage1, 'all');
 STATS2 = regionprops(colorByNumbers, fluorImage2, 'MeanIntensity');
 
 Centroid = zeros(cellsNumber, 2);
 
-[neigborList, neigborCount, edgecellss] = findNeigbors(STATS, colorByNumbers);
+[neigborList, neigborCount, edgecellss] = findNeighbors(STATS, colorByNumbers);
 
-%% Create cells structure and store statistics
-
-
+%% Create "cells" data structure and store statistics
 for i = cellsNumber:-1:1
-    cells(i).area = STATS(i).Area / pixelRatio^2;
+    cells(i).area = STATS(i).Area / pixelRatio^2; % focus
     cells(i).dpERKIntensity = STATS(i).MeanIntensity;
     cells(i).aspectRatio = STATS(i).MajorAxisLength / STATS(i).MinorAxisLength;
     cells(i).perimeter = STATS(i).Perimeter / pixelRatio;
     cells(i).convexArea = STATS(i).ConvexArea / pixelRatio^2;
-    cells(i).solidity = cells(i).convexArea / cells(i).area;
     cells(i).majorAxis = STATS(i).MajorAxisLength / pixelRatio;
     cells(i).minorAxis = STATS(i).MinorAxisLength / pixelRatio;
     cells(i).engrailedLevel = STATS2(i).MeanIntensity;
@@ -114,41 +97,28 @@ for i = cellsNumber:-1:1
 end
 
 %% Calculate Regional Averages and Neighbor Statistics
-
+% everything in "no average" does not get averaged, probably should be
+% separate structure
 cellFields = fieldnames(cells);
 
-for m = 1:length(cells)
-    for i = 2:length(cellFields)
-        for j = 1:(i-1)
-            if ~strcmp(cellFields{i},'noAverage') && ~strcmp(cellFields{j},'noAverage')
-                cotemp = cells(m).(cellFields{i}) ./ cells(m).(cellFields{j});
-                cells(m).([cellFields{i} 'over' cellFields{j}]) = cotemp;
+for k = 1:numel(cellFields)
+    if ~strcmp(cellFields{k},'noAverage')
+        for i = cellsNumber:-1:1
+            cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) = cells(neigborList(i,1)).(cell2mat(cellFields(k)));
+            cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) = cells(neigborList(i,1)).(cell2mat(cellFields(k))) + cells(i).(cell2mat(cellFields(k)));
+            for j = neigborList(i,2:neigborCount(i))
+                cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) = cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) + cells(j).(cell2mat(cellFields(k)));
+                cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) = cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) + cells(j).(cell2mat(cellFields(k)));
             end
+            cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) = cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) ./ neigborCount(i);
+            cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) = cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) ./ (neigborCount(i)+1);
+            cells(i).noAverage.relativeValue.(cell2mat(cellFields(k))) = cells(i).(cell2mat(cellFields(k))) ./ cells(i).noAverage.neighborAve.(cell2mat(cellFields(k)));
         end
     end
 end
 
-
-% for k = 1:numel(cellFields)
-%     if ~strcmp(cellFields{k},'noAverage')
-%         for i = cellsNumber:-1:1
-%             cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) = cells(neigborList(i,1)).(cell2mat(cellFields(k)));
-%             cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) = cells(neigborList(i,1)).(cell2mat(cellFields(k))) + cells(i).(cell2mat(cellFields(k)));
-%             for j = neigborList(i,2:neigborCount(i))
-%                 cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) = cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) + cells(j).(cell2mat(cellFields(k)));
-%                 cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) = cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) + cells(j).(cell2mat(cellFields(k)));
-%             end
-%             cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) = cells(i).noAverage.neighborAve.(cell2mat(cellFields(k))) ./ neigborCount(i);
-%             cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) = cells(i).noAverage.regionalAve.(cell2mat(cellFields(k))) ./ (neigborCount(i)+1);
-%             cells(i).noAverage.relativeValue.(cell2mat(cellFields(k))) = cells(i).(cell2mat(cellFields(k))) ./ cells(i).noAverage.neighborAve.(cell2mat(cellFields(k)));
-%         end
-%     end
-% end
-
-
-
 %% Create connection matricies for network statistics
-
+% which cells are connected to which cells - for network statistics
 D = pdist(cat(1,Centroid(:,1),Centroid(:,2)));
 distance = squareform(D) * pixelRatio;
 
@@ -157,7 +127,7 @@ cMatrixWeighted = zeros(cellsNumber);
 
 for i = 1:cellsNumber
     neigbors = neigborList(i,1:neigborCount(i));
-    for j = neigbors(neigbors>0)
+    for j = neigbors
         cMatrixUnweighted(i,j) = 1;
         cMatrixWeighted(i,j) = distance(i,j);
     end
@@ -166,19 +136,18 @@ end
 save([outputfolder 'cMatrixUnweighted.mat'], 'cMatrixUnweighted');
 save([outputfolder 'cMatrixWeighted.mat'], 'cMatrixWeighted');
 
-
 %% Perform Segmentation
-
-
-
-
+% obtain groups of cells manually, should be replaced with Sarah's script
 segmentBelongedTo = cell(cellsNumber, 1);
 figureHandle = imshow(compositeMap);
 
-x = 1; y = 1; notDone = true; segmentList = segmentListMaster;
+x = 1; 
+y = 1; 
+notDone = true; 
+segmentList = segmentListMaster;
 identificationArray = cell(1, cellsNumber);
 
-
+% Replace with Sarah's tool
 while notDone && ~isempty(segmentList)
     
     [selection, notDone] = listdlg('ListString',segmentList,'SelectionMode','single','PromptString','Name of Current Segment');
@@ -186,10 +155,14 @@ while notDone && ~isempty(segmentList)
     segmentList(selection) = [];
     
     if notDone
-        [x,y] = ginput();
+        [x,y] = ginputc('Color', 'g', 'ShowPoints', true, 'ConnectPoints', true);
+        global storedPoints;
+        %line([storedPoints(1,1), storedPoints(end,1)], [storedPoints(1,2), storedPoints(end,2)]);
         [ axyp, ~, ~ ] = polygeom( x, y );
         compositeMap = insertText(compositeMap, axyp(2:3), currentSegment, 'FontSize', 18, 'TextColor', ...
             [255 255 255], 'AnchorPoint', 'Center', 'BoxOpacity', 0);
+        compositeMap = insertShape(compositeMap, 'Line', [storedPoints(1,1), storedPoints(1,2), storedPoints(end,1), storedPoints(end,2)], 'Color', 'y');
+        % insertShape(..., 'FilledPolygon', ...)
         figureHandle = imshow(compositeMap);
         in = inpoly(Centroid, [x y]);
         identificationArray(in) = {currentSegment};
@@ -197,8 +170,10 @@ while notDone && ~isempty(segmentList)
     
 end
 
+% any cell that hasn't been identified into a compartment is assigned to
+% the closest compartment - maybe not best strategy
 unidentified = double(cellfun('isempty',identificationArray));
-unidentified = unidentified .* double(1:cellsNumber);
+unidentified = unidentified .* (1:cellsNumber);
 unidentified(unidentified == 0) = [];
 
 while ~isempty(unidentified)
@@ -223,113 +198,36 @@ while ~isempty(unidentified)
 end
 
 for i = cellsNumber:-1:1
-    cells(i).segmentBelongedTo = identificationArray{i};
+    cells(i).segmentBelongedTo = identificationArray{i}; % focus
     cells(i).subSegmentBelongedTo = cells(i).segmentBelongedTo;
 end
 
 %% Create cell-level macrosegmentation map
-
 for i = length(segmentListMaster):-1:1
     macroSeg(ismember(identificationArray, segmentListMaster{i})) = i;
 end
 
+% put dot in each cell and color sections 
 createScatterPlots(Centroid(:,1), Centroid(:,2), macroSeg, 'Segmentation Map', '')
 
-
-cellFields = fieldnames(cells);
-
-identities = unique(identificationArray);
-
- for i = 2:length(cellFields)
-        for j = 1:(i-1)
-            if ~strcmp(cellFields{i},'noAverage') && ~strcmp(cellFields{j},'noAverage')
-            cotemp = corrcoef(double([cells.(cellFields{i})]),double([cells.(cellFields{j})]));
-            population.(['corr' int2str(i) 'vs' int2str(j)]) = cotemp(1,2);
-            end
-            end
-    end
-
-for currentPop = 1:max(macroSeg);
-    for i = 1:length(cellFields)
-        if ~strcmp(cellFields{i},'noAverage')
-            population(currentPop).(['average' cellFields{i}]) = mean([cells(macroSeg==currentPop).(cellFields{i})]);
-            population(currentPop).(['stdev' cellFields{i}]) = std([cells(macroSeg==currentPop).(cellFields{i})]);
-        end
-    end
-    
-   
-end
-
-
 %% Run PCA Analysis
-
 cellPCA(cells);
 
-%% Identify sub-segments computationally
-
-% segmentsToSplitEngrailed = {'Body','Head'};
-%  segmentsToSplitEngrailed = {'C5'; ...
-%      'dorsal T1';'dorsal T2';'dorsal T3';'dorsal A1';'dorsal A2';'dorsal A3'; ...
-%      'dorsal A4';'dorsal A5'};
-%
-% identificationSubArray = cell(1, cellsNumber);
-%
-% for currentSegmentNumber = 1:length(segmentsToSplitEngrailed)
-%     currentSegment = segmentsToSplitEngrailed(currentSegmentNumber);
-%     currentCells = find(ismember(identificationArray, currentSegment));
-%     activeCells = cells(currentCells);
-%
-%     X = [];
-%
-%     for i = length(activeCells):-1:1
-%         X(i,1:2) = activeCells(i).noAverage.centroid;
-%         X(i,3) = activeCells(i).engrailedLevel;
-%     end
-%
-%     cluster = kmeans(X,2);
-%     pCluster = find(ismember(cluster, 1));
-%     aCluster = find(ismember(cluster, 2));
-%
-%     PNumber = [];
-%     ANumber = [];
-%
-%     for i = pCluster'
-%         PNumber = cat(2,PNumber,activeCells(i).noAverage.number);
-%     end
-%
-%     for i = aCluster'
-%         ANumber = cat(2,ANumber,activeCells(i).noAverage.number);
-%     end
-%
-%     for i = PNumber
-%       cells(i).subSegmentBelongedTo = {[cells(i).subSegmentBelongedTo ' P Compartment']};
-%     end
-%
-%     for i = ANumber
-%         cells(i).subSegmentBelongedTo = {[cells(i).subSegmentBelongedTo ' A Compartment']};
-%     end
-%
-% end
-
 %% Run all possible p tests
-
-%p tests between all properties for every segment
-p_dpERK = cellTTest([cells.dpERKIntensity],[{cells.segmentBelongedTo}]);
-p_engrailedLevel = cellTTest([cells.engrailedLevel],[{cells.segmentBelongedTo}]);
-p_area = cellTTest([cells.area],[{cells.segmentBelongedTo}]);
-p_polygonClass = cellTTest([cells.polygonClass],[{cells.segmentBelongedTo}]);
+% p tests between all properties for every segment
+p_dpERK = cellTTest([cells.dpERKIntensity], {cells.segmentBelongedTo});
+p_engrailedLevel = cellTTest([cells.engrailedLevel], {cells.segmentBelongedTo});
+p_area = cellTTest([cells.area], {cells.segmentBelongedTo});
+p_polygonClass = cellTTest([cells.polygonClass], {cells.segmentBelongedTo});
 
 p_sub_dpERK = cellTTest([cells.dpERKIntensity],[cells.subSegmentBelongedTo]);
 p_sub_engrailedLevel = cellTTest([cells.engrailedLevel],[cells.subSegmentBelongedTo]);
 p_sub_area = cellTTest([cells.area],[cells.subSegmentBelongedTo]);
 p_sub_polygonClass = cellTTest([cells.polygonClass],[cells.subSegmentBelongedTo]);
 
-
-
 save([outputfolder 'pTests.mat'], 'p_*');
 
 %% Output Tables
-
 OutputTable = cell(cellsNumber+1, numel(cellFields)+1);
 OutputTable(2:(cellsNumber+1),1) = num2cell(1:cellsNumber);
 
